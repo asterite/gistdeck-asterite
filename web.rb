8beabb39bc587f13ec052ad3eb2aa9b68f088c10
Ary Borenszweig <aborenszweig@manas.com.ar>
2012-09-28 16:31:12 -0300

    Tortoise and hare

--- web.rb
+++ web.rb
@@ -1,5 +1,28 @@
 require 'sinatra'
+require 'rest-client'
 
-get '/' do
-  "Hello, world"
+JS = "
+  GISTDECK_CSS_URL = '/gistdeck.css';
+
+  function addListener(obj, eventName, listener) {
+    if(obj.addEventListener) {
+      obj.addEventListener(eventName, listener, false);
+    } else {
+      obj.attachEvent('on' + eventName, listener);
+    }
+  }
+
+  addListener(document, 'DOMContentLoaded', function() {
+    var s = document.createElement('script');
+    s.setAttribute('src','https://raw.github.com/asterite/gistdeck/master/gistdeck.js');
+    document.getElementsByTagName('head')[0].appendChild(s);
+  });
+"
+SCRIPT = %Q(<script type="text/javascript">#{JS}</script>)
+
+get '/:gist_id' do |gist_id|
+  body = RestClient.get "https://gist.github.com/#{gist_id}"
+  index = body.index '</body>'
+
+  body[0 ... index] + SCRIPT + body[index .. -1]
 end
